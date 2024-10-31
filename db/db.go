d22df55bdcd9ec5e59e9fbb66661edbb2e8c0559
Dung Tran <garrickedd@gmail.com>
2024-10-31 10:05:53 +0700

    feat: connect to db & auto-migration

--- db/db.go
+++ db/db.go
@@ -1,14 +1,19 @@
 package db
 
 import (
-	
+	"github.com/garrickedd/ReLibca/src/api/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-const (
-	dir = "db/migrations"
-)
+var DB *gorm.DB
 
-func main() {
+func Connect() {
+	db, err := gorm.Open(postgres.Open("postgres://postgres:admin@localhost:5432/relibcadb"))
+	if err != nil {
+		panic(err)
+	}
 
-
+	db.AutoMigrate(&models.User{})
+	DB = db
 }
