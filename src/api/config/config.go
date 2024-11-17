a3c2a8ccab728265fae212f01735cc55e70c418f
Dung Tran <garrickedd@gmail.com>
2024-11-18 00:28:23 +0700

    feat: Create postgres db and connect

---
+++ src/api/config/config.go
@@ -0,0 +1,83 @@
+package config
+
+import (
+	"errors"
+	"log"
+	"os"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Server   ServerConfig
+	Postgres PostgresConfig
+}
+
+type ServerConfig struct {
+	InternalPort string
+	ExternalPort string
+	RunMode      string
+}
+
+type PostgresConfig struct {
+	Host            string
+	Port            string
+	User            string
+	Password        string
+	DbName          string
+	SSLMode         string
+	MaxIdleConns    int
+	MaxOpenConns    int
+	ConnMaxLifetime time.Duration
+}
+
+func GetConfig() *Config {
+	cfgPath := getConfigPath(os.Getenv("APP_ENV"))
+	v, err := LoadConfig(cfgPath, "yml")
+	if err != nil {
+		log.Fatalf("Error in load config %v", err)
+	}
+
+	cfg, err := ParseConfig(v)
+
+	if err != nil {
+		log.Fatalf("Error in parse config %v", err)
+	}
+
+	return cfg
+}
+
+func ParseConfig(v *viper.Viper) (*Config, error) {
+	var cfg Config
+	err := v.Unmarshal(&cfg)
+	if err != nil {
+		log.Printf("Unable to parse config: %v", err)
+		return nil, err
+	}
+	return &cfg, nil
+}
+
+func LoadConfig(filename string, fileType string) (*viper.Viper, error) {
+	v := viper.New()
+	v.SetConfigType(fileType)
+	v.SetConfigName(filename)
+	v.AddConfigPath(".")
+	v.AutomaticEnv()
+	err := v.ReadInConfig()
+	if err != nil {
+		log.Printf("Unable to read config: %v", err)
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			return nil, errors.New("config file not found")
+		}
+		return nil, err
+	}
+	return v, nil
+}
+
+func getConfigPath(env string) string {
+	if env != "" {
+		return "src/api/config/config-development"
+	}
+	return "src/api/config/config-development"
+}
--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"log"
+
+	"github.com/garickedd/ReLibca/src/api"
+	"github.com/garickedd/ReLibca/src/api/config"
+	database "github.com/garickedd/ReLibca/src/infrastructure/persistence"
+)
+
+func main() {
+	cfg := config.GetConfig()
+
+	err := database.InitDb(cfg)
+	defer database.CloseDb()
+	if err != nil {
+		log.Fatal()
+	}
+
+	api.InitServer()
+}
