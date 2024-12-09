a39798f91efc4d4280f358941f2a3d4394d04651
Dung Tran <garrickedd@gmail.com>
2024-12-09 22:28:06 +0700

    feat: setup database

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"github.com/garrickedd/ReLibca/src/infrastructure"
+)
+
+func main() {
+	database, err := infrastructure.Postgres()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(database)
 }
---
+++ src/infrastructure/postgres.go
@@ -0,0 +1,45 @@
+package infrastructure
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func Postgres() (*sqlx.DB, error) {
+	err := godotenv.Load("/mnt/d/Engineer/Server-adv/Project/ReLibca/.env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	host := os.Getenv("POSTGRES_HOST")
+	user := os.Getenv("POSTGRES_USER")
+	pass := os.Getenv("POSTGRES_PASSWORD")
+	dbName := os.Getenv("POSTGRES_DB")
+	port := os.Getenv("POSTGRES_PORT")
+
+	config := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, pass, dbName, port)
+
+	// fmt.Println(config)
+	// // return sqlx.Open("postgres", config)
+	return sqlx.Connect("postgres", config)
+
+	// // Kết nối PostgreSQL
+	// db, err := sqlx.Connect("postgres", config)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// // Kiểm tra kết nối
+	// err = db.Ping()
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// fmt.Println("Kết nối PostgreSQL thành công!")
+	// return db, nil
+}
